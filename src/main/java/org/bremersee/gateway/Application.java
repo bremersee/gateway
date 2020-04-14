fce79c5b7f01f7ccfaff1cd157bd6bc179c3d1dd
Christian Bremer <bremersee@googlemail.com>
2020-04-14 13:27:53 +0200

    Init

--- src/main/java/org/bremersee/gateway/Application.java
+++ src/main/java/org/bremersee/gateway/Application.java
@@ -2,9 +2,12 @@ package org.bremersee.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
 
 @SpringBootApplication
 public class Application {
+
+	RouteDefinition rd;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
