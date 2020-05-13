53d06379995749324cf94ed71a273c10720c1da1
Christian Bremer <bremersee@googlemail.com>
2020-05-13 23:11:02 +0200

    Version 1.0.1-SNAPSHOT

--- src/test/java/org/bremersee/gateway/ApplicationTests.java
+++ src/test/java/org/bremersee/gateway/ApplicationTests.java
@@ -39,7 +39,7 @@ import org.springframework.util.StringUtils;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
     "bremersee.security.authentication.enable-jwt-support=false"
 })
-@ActiveProfiles({"basic-auth"})
+@ActiveProfiles({"in-memory"})
 @TestInstance(Lifecycle.PER_CLASS)
 class ApplicationTests {
 
