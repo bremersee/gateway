ed79d4753494fccb151572443651433be7481dcf
Jenkins <jenkins@jenkins.bremersee.lokal>
2020-05-14 19:11:52 +0200

    Site checkin for project gateway

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.bremersee.gateway"},{"l":"org.bremersee.gateway.config"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.bremersee.gateway"}]
